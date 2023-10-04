4547bc6b1c446b9d1b4900f18dd6d9dc6b22fea4
MegaraWan <chaioyazhou@gmail.com>
2023-10-05 03:00:48 +0800

    修改 放大 只有在桌機有用

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+function isSmallScreen() {
+    return window.innerWidth >= 1024;
+}
+
 function printPage() {
     window.print();
 }
+
 const more = document.querySelectorAll(".skill-img");
 const moreItems = document.querySelectorAll(".more-item");
 const closeButtons = document.querySelectorAll(".close");
@@ -8,23 +13,29 @@ const closeButtons = document.querySelectorAll(".close");
 // 顯示 more-item
 more.forEach((button, index) => {
     button.addEventListener("click", () => {
-        moreItems[index].style.display = "block";
+        if (isSmallScreen()) {
+            moreItems[index].style.display = "block";
+        }
     });
 });
 
 // 隱藏 more-item
 closeButtons.forEach((closeButton, index) => {
     closeButton.addEventListener("click", () => {
-        moreItems[index].style.display = "none";
+        if (isSmallScreen()) {
+            moreItems[index].style.display = "none";
+        }
     });
 });
 
 //點擊視窗其他地方隱藏 more-item
 window.onclick = function(event) {
-    moreItems.forEach((moreItem) => {
-        // 檢查點擊的元素是否是 moreItem 或其子元素
-        if (!moreItem.contains(event.target) && event.target !== moreItem && !event.target.classList.contains("skill-img")) {
-            moreItem.style.display = "none";
-        }
-    });
+    if (isSmallScreen()) {
+        moreItems.forEach((moreItem) => {
+            // 檢查點擊的元素是否是 moreItem 或其子元素
+            if (!moreItem.contains(event.target) && event.target !== moreItem && !event.target.classList.contains("skill-img")) {
+                moreItem.style.display = "none";
+            }
+        });
+    }
 };
